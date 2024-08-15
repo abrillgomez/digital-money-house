97a72d316fd91e28b02b5e1f31b67d3c17549eac
abrillgomez <abrilgomezp@outlook.com>
2024-08-14 21:36:36 -0300

    login y layout

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,26 +1,39 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const bgColor = pathname === "/login" ? "bg-lime-500" : "bg-black";
+  const logo =
+    pathname === "/login" ? "/assets/logo-negro.png" : "/assets/logo-verde.png";
+
   return (
-    <div className="bg-custom-dark h-[64px] w-full flex justify-between items-center px-4">
+    <div
+      className={`${bgColor} h-[64px] w-full flex justify-between items-center px-4`}>
       <div className="text-white font-bold">
         <Link href="/">
           <img
-            src="/assets/logo-verde.png"
+            src={logo}
             alt="Logo de Digital Money House"
             className="h-7 w-auto mr-4"
           />
         </Link>
       </div>
-      <div className="flex space-x-4">
-        <button className="text-[14px] bg-transparent text-custom-lime px-4 py-2 rounded border border-custom-lime font-bold">
-          Ingresar
-        </button>
-        <button className="text-[14px] bg-custom-lime text-black px-4 py-2 rounded font-bold">
-          Crear cuenta
-        </button>
-      </div>
+      {pathname !== "/login" && (
+        <div className="flex space-x-4">
+          <Link href="/login">
+            <div className="text-[14px] bg-transparent text-custom-lime px-4 py-2 rounded border border-custom-lime font-bold">
+              Ingresar
+            </div>
+          </Link>
+          <button className="text-[14px] bg-custom-lime text-black px-4 py-2 rounded font-bold">
+            Crear cuenta
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import Navbar from "./components/navbar/Navbar";
 import LandingPage from "./components/landingPage/LandingPage";
-import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
     <>
       <div>
-        <Navbar />
         <LandingPage />
-        <Footer />
       </div>
     </>
   );
